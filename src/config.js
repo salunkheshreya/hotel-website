e27eddc5b4e56408f188fb8f4b1f5e0163b7efc8
Shreya Salunkhe <shreyasalunkhe1208@gmail.com>
2026-03-28 00:25:50 +0530

    done

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const config = {
-    API_BASE_URL: "http://localhost/hotel-website",
+    API_BASE_URL: process.env.REACT_APP_API_URL || "http://localhost/hotel-website",
 };
 
 export default config;
